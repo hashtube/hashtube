c8a5776e58e720fc2cc9b3c5c6df310371d37408
Bao D <baocancode@gmail.com>
2019-08-17 00:13:00 +1000

    dev: add cypress for e2e test

---
+++ cypress/integration/api/demo.test.js
@@ -0,0 +1,20 @@
+describe('/api/demo', () => {
+  describe('GET /video/:id', () => {
+    it('should return the video', () => {
+      cy.request('http://localhost:4100/demo/video/Bv_5Zv5c-Ts')
+        .then((response) => {
+          const video = response.body
+          expect(video.id).to.equal('Bv_5Zv5c-Ts')
+          expect(video.publishedAt).to.have.string('2015-04-03')
+          expect(video.title).to.have.string('JavaScript')
+          expect(video.description).to.have.string('JavaScript')
+          expect(video.tags).to.not.be.empty
+          expect(video.duration).to.equal('PT3H32M50S')
+          expect(video.thumbnail).to.not.be.empty
+          expect(video.viewCount).to.be.above(1000000)
+          expect(video.likeCount).to.be.above(10000)
+          expect(video.dislikeCount).to.be.above(100)
+        })
+    })
+  })
+})
